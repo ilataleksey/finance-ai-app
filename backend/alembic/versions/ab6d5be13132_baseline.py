fc40ad24fec95440257a271edf652ae10251eb9a
ilataleksey <ilataleksey@gmail.com>
2026-07-28 22:38:00 +0300

    fix: align budget schema with models

---
+++ backend/alembic/versions/ab6d5be13132_baseline.py
@@ -0,0 +1,28 @@
+"""baseline
+
+Revision ID: ab6d5be13132
+Revises: 
+Create Date: 2026-07-28 22:21:25.323861
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'ab6d5be13132'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
